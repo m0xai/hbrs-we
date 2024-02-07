a101ebb7fc742eb264b5b356083605e66baa4d63
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-02-07 10:59:37 +0100

    feat: add solution for task 2.4

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
 import { toggleNav } from "./sidebar.js";
 
 window.toggleNav = toggleNav;
+
+const stats = document.getElementById("stats")
+
+const secondsEl = document.createElement("p");
+let secondsSince = 0;
+setInterval(() => {
+    secondsSince++;
+    secondsEl.innerText = `You're viewing this page since ${secondsSince} seconds`;
+}, 1000);
+
+stats.appendChild(secondsEl)
